86b182706c75ba34b0d5d5372aa00474781f59f4
Ananthu Sivakumar <ananthusivakumar0@gmail.com>
2026-03-04 19:42:33 +0530

    Updated admin page

--- Admin/create_admin.php
+++ Admin/create_admin.php
@@ -1,11 +1,14 @@
+
+
 <?php
 include __DIR__ . '/../includes/db.php';
 
-$username = "admin";
-$password = password_hash("123456", PASSWORD_DEFAULT);
+$username = "green_tech";
+$password = password_hash("grean@26", PASSWORD_DEFAULT);
 
 $stmt = $conn->prepare("INSERT INTO admins (username, password) VALUES (?, ?)");
 $stmt->bind_param("ss", $username, $password);
 $stmt->execute();
 
-echo "Admin Created Successfully!";
+
+
